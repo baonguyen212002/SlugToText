ce64da8874fb2b6326449b78e35fd8710d400ff5
Sekai <baonguyen212002@gmail.com>
2025-02-04 08:32:27 +0700

    update darkmode

--- public/app.js
+++ public/app.js
@@ -8,7 +8,27 @@ function convertSlug() {
 }
 function copyToClipboard() {
     const textToCopy = document.getElementById('convertedText').innerText;
-    if (textToCopy) { 
+    if (textToCopy) {
         navigator.clipboard.writeText(textToCopy);
     }
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+    const darkModeToggle = document.getElementById("darkModeToggle");
+    const isDarkMode = localStorage.getItem("darkMode") === "enabled";
+
+    if (isDarkMode) {
+        document.body.classList.add("dark-mode");
+        darkModeToggle.checked = true;
+    }
+
+    darkModeToggle.addEventListener("change", function () {
+        if (darkModeToggle.checked) {
+            document.body.classList.add("dark-mode");
+            localStorage.setItem("darkMode", "enabled");
+        } else {
+            document.body.classList.remove("dark-mode");
+            localStorage.setItem("darkMode", "disabled");
+        }
+    });
+});
